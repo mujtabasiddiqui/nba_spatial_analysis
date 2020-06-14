f5a5a452cd9a31a2c3bc84045a4da3321b431107
mujtabasiddiqui <mujtabasiddiqui@live.ca>
2020-06-14 17:03:33 -0400

    Updated requirements and app init

---
+++ app/dash_app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
