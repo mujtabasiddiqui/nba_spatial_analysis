37ed732763e225adaf824649ea10defd957ee3ec
mujtabasiddiqui <mujtabasiddiqui@live.ca>
2020-07-02 23:47:35 -0400

    Dash App template update

--- app/dash_app.py
+++ app/dash_app.py
@@ -1,3 +1,4 @@
+##### Imports ####
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
@@ -5,10 +6,26 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import numpy as np 
+from nba_api.stats.static import players
+from nba_api.stats.static import teams
+from nba_api.stats.endpoints import shotchartdetail
+from nba_api.stats.endpoints import playercareerstats
+import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import scipy.stats as st
 
-app = dash.Dash(__name__)
-server = app.server
+##### Helper Functions ########
 
-app.layout = html.Div([
-    
-])
+
+
+##### Dash Layout #######
+
+
+##### Dash Callback Functions #####
+
+
+##### Run ######
+if __name__ == '__main__':
+    app.run_server(debug=True)
