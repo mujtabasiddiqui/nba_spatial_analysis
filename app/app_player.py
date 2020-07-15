f45f45aec953a95f240b43586d9703da69e37a56
mujtabasiddiqui <mujtabasiddiqui@live.ca>
2020-07-14 23:10:16 -0400

    Added player profile

--- app/app_player.py
+++ app/app_player.py
@@ -11,11 +11,13 @@ from nba_api.stats.static import players
 from nba_api.stats.static import teams
 from nba_api.stats.endpoints import shotchartdetail
 from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.endpoints import commonplayerinfo
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import scipy.stats as st
 import requests
+from datetime import date
 
 ####### Analysis, Visualization, Data Pull Functions ############
 def get_players_list():
@@ -244,56 +246,124 @@ def make_heatmap(fig, shots_df, name, season_id):
             'xanchor': 'center',
             'yanchor': 'top'})
 
-def make_player_hexbin(fig, shots_df,name,season_id):
-    Hex = plt.hexbin(x=shots_df['LOC_X'], y=shots_df['LOC_Y'],gridsize=25,vmin = 0.0, vmax = 0.7,
-    cmap=plt.get_cmap('YlOrRd'), mincnt=3)
+def make_hexbin(fig, shots_df,league_avg,in_type,name,season_id):
+    
+    grid_size= 40 
+    min_show = max(2, round(len(shots_df)* 0.001)) # min count for shots in hex
+    
+    x_loc = []
+    y_loc = []
+    y_limit = 300
 
-    HexC = plt.hexbin(x=shots_df['LOC_X'], y=shots_df['LOC_Y'], C=shots_df['SHOT_MADE_FLAG'],gridsize=25,vmin = 0.0, vmax = 0.7, 
-    cmap=plt.get_cmap('YlOrRd'), mincnt=2)
+    for y in range(len(shots_df['LOC_Y'])):
+        if shots_df['LOC_Y'][y] < y_limit:
+            y_loc.append(shots_df['LOC_Y'][y])
+            x_loc.append(shots_df['LOC_X'][y])
+    
+    
+    Hex = plt.hexbin(x=x_loc, y=y_loc,gridsize=grid_size,vmin = 0.0, vmax = 0.7,
+    cmap=plt.get_cmap('YlOrRd'), mincnt= min_show + 1)
 
+    # join shotchart with legue average
+    la_basic = league_avg['SHOT_ZONE_BASIC']
+    la_area = league_avg['SHOT_ZONE_AREA']
+    la_range = league_avg['SHOT_ZONE_RANGE']
+    la_fg_pct = league_avg['FG_PCT']
+    la_fg_pct_player = []
+    shot_l = len(shots_df)
+    la_l = len(league_avg)
+
+    
+    for i in range(shot_l):
+        for j in range(la_l):
+            if shots_df['SHOT_ZONE_BASIC'][i] == la_basic[j] and \
+                shots_df['SHOT_ZONE_AREA'][i] == la_area[j] and \
+                shots_df['SHOT_ZONE_RANGE'][i] == la_range[j]:
+                la_fg_pct_player.append(la_fg_pct[j])
+                break
+
+    # Make hex distribution of the legue average FG % 
+    HexL = plt.hexbin(x=x_loc, y=y_loc, C=la_fg_pct_player,gridsize=grid_size, vmin = 0.0, vmax = 0.7, 
+    cmap=plt.get_cmap('YlOrRd'), mincnt=min_show)
+
+    # Make hex distribution of the palyer FG % 
+    HexC = plt.hexbin(x=x_loc, y=y_loc, C=shots_df['SHOT_MADE_FLAG'],gridsize=grid_size, vmin = 0.0, vmax = 0.7, 
+    cmap=plt.get_cmap('YlOrRd'), mincnt=min_show)
+
+    # Extract data from hexbins
     loc = HexC.get_offsets()
     acc = HexC.get_array()
+    la_acc = HexL.get_array()
     shots = Hex.get_array()
 
     xlocs = []
     ylocs = []
     accs_by_hex = []
+    la_accs_by_hex = []
+    diff_by_hex = []
     shots_by_hex = []
+    hex_size = []
 
     for i in range(len(loc)):
         xlocs.append(loc[i][0])
         ylocs.append(loc[i][1])
         accs_by_hex.append(acc[i])
+        la_accs_by_hex.append(la_acc[i])
+        diff_by_hex.append(acc[i] - la_acc[i])
         shots_by_hex.append(shots[i])
 
     #freq is % of total shots
-    freq_by_hex= list(map(lambda x: x/len(shots_df), shots_by_hex))
+    freq_by_hex = list(map(lambda x: x/len(shots_df), shots_by_hex))
+    hex_size = list(map(lambda x: x * 4, freq_by_hex))
 
-    hexbin_text = [
-        '<i>Accuracy: </i>' + str(round(accs_by_hex[i]*100, 1)) + '%<BR>'
-        '<i>Frequency: </i>' + str(round(freq_by_hex[i]*100, 2)) + '%'
-        for i in range(len(freq_by_hex))
-    ]
-
+    if in_type == 'p':
+        hexbin_text = [
+            '<i>Legue FG: </i>' + str(round(la_accs_by_hex[i]*100, 1)) + '%<BR>'
+            '<i>Player FG: </i>' + str(round(accs_by_hex[i]*100, 1)) + '%<BR>'
+            '<i>Difference: </i>' + str(round(diff_by_hex[i]*100, 1)) + '%<BR>'
+            '<i>Frequency: </i>' + str(round(freq_by_hex[i]*100, 2)) + '%'
+            for i in range(len(freq_by_hex))
+        ]
+        
+    elif in_type == 't':
+        hexbin_text = [
+            '<i>Legue FG: </i>' + str(round(la_accs_by_hex[i]*100, 1)) + '%<BR>'
+            '<i>Team FG: </i>' + str(round(accs_by_hex[i]*100, 1)) + '%<BR>'
+            '<i>Difference: </i>' + str(round(diff_by_hex[i]*100, 1)) + '%<BR>'
+            '<i>Frequency: </i>' + str(round(freq_by_hex[i]*100, 2)) + '%'
+            for i in range(len(freq_by_hex))
+        ]
+        
+    max_val = max(diff_by_hex) - max(diff_by_hex) * 0.2
+    min_val = min(diff_by_hex)
     fig.add_trace(go.Scatter(x=xlocs, y=ylocs, mode='markers', name='markers', 
-                        marker=dict(size=freq_by_hex, sizemode='area', sizeref= 2. * max(freq_by_hex) / (18. ** 2), 
-                                    sizemin=4.5,color=accs_by_hex, colorscale='YlOrRd',line=dict(width=1, color='#333333'),
+                        marker=dict(size=freq_by_hex, sizemode='area', sizeref= 2. * max(freq_by_hex) / (12. ** 2), 
+                                    sizemin=3.,color=diff_by_hex, colorscale="RdYlBu",line=dict(width=1, color='black'),reversescale=True,
                                     colorbar=dict(thickness=15, x=0.84,y=0.87, yanchor='middle',len=0.2,
-                                                    title=dict(text="<B>Accuracy</B>",font=dict(size=11,color='#4d4d4d'))),
+                                                    title=dict(text="<B>vs League Avg</B>",font=dict(size=10,color='black')),
+                                                    tickvals=[max_val, 0, min_val], 
+                                                    ticktext=["Better", "On par", "Worse"]),
                                     symbol='hexagon'), text=hexbin_text, hoverinfo='text'))
 
     fig.update_layout(
         title={
-            'text': f"{name} {season_id} Shot Heatmap",
+            'text': f"{name} {season_id} Hex Shot Map",
             'y':0.98,
             'x':0.5,
             'xanchor': 'center',
-            'yanchor': 'top'})
+            'yanchor': 'top'
+            },
+         font=dict(color="black"))
     
 
 ####### Dash Layout ###########
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', 
+                        'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.scripts.append_script({'external_url': 'https://code.jquery.com/jquery-3.5.1.slim.min.js'})
+app.scripts.append_script({'external_url': 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'})
+app.scripts.append_script({'external_url': 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js'})
 
 app.layout = html.Div([
     #Search Bars
@@ -347,14 +417,53 @@ app.layout = html.Div([
 
     #Player Stats Top Right
     html.Div(children=[
-        html.Div([
-            html.Img(id='player_img', style={'width': '30%','paddingLeft': '2%', 'display': 'inline-block'}),
 
+        
+        html.Div([
             html.Div([
-                html.H3(id='player_name')
-            ])
-        ], style={'background': '#1975FA', 'border-radius':'5px'})
-    ],style={'width': '50%', 'float': 'right', 'display': 'inline-block'})
+                html.Img(id='player_img', style={'width': '30%','paddingLeft': '2%', 'display': 'inline-block', 'float': 'left', 'height': '100%'}),
+
+                html.Table([
+                    html.Tbody([
+                        html.Tr([
+                            html.Td(id='timeframe', style={'border': '1px solid white','color': 'white'}, colSpan=4)
+                        ]),
+
+                        html.Tr([
+                            html.Td(id='player_pts', style={'border': '1px solid white','color': 'white'}),
+                            html.Td(id='player_reb', style={'border': '1px solid white','color': 'white'}),
+                            html.Td(id='player_ast', style={'border': '1px solid white','color': 'white'}),
+                            html.Td(id='player_pie', style={'border': '1px solid white','color': 'white'}),
+                        ])
+                    ])
+                ])
+            ]),
+            
+            html.Div([
+                html.H3(id='player_name', style={'color':'white'}),
+                html.Img(id='team_logo', style={'width':'5%'}),
+                html.Div(id='player_team_pos', style={'display': 'inline-block', 'paddingLeft': '2%', 'color': 'white'})
+            ]),
+
+            html.Table([
+                html.Tbody([
+                    html.Tr([
+                        html.Td(id='player_ht', style={'border': '1px solid white','color': 'white'}),
+                        html.Td(id='player_wt', style={'border': '1px solid white','color': 'white'}),
+                        html.Td(id='player_prior', style={'border': '1px solid white','color': 'white', 'border-right': 'None'}, colSpan=2),
+                    ]),
+
+                    html.Tr([
+                        html.Td(id='player_age', style={'border': '1px solid white','color': 'white', 'border-bottom': 'None'}),
+                        html.Td(id='player_born', style={'border': '1px solid white','color': 'white', 'border-bottom': 'None'}),
+                        html.Td(id='player_draft', style={'border': '1px solid white','color': 'white', 'border-bottom': 'None'}),
+                        html.Td(id='player_exp', style={'border': '1px solid white','color': 'white', 'border-right': 'None', 'border-bottom': 'None'})
+                    ])
+                ])
+            ], style={'height':'50%'})
+        ], style={'width': '60%','paddingLeft': '2%', 'display': 'inline-block', 'float': 'right'})
+
+    ],style={'width': '50%', 'float': 'right', 'display': 'inline-block','background': '#1975FA', 'border-radius':'5px'})
 ])
 
 ###### Callback Dash Functions ##########
@@ -393,7 +502,7 @@ def display_shot_charts(player, season, season_type):
         make_heatmap(heat_fig,shots_df,name,season)
         draw_plotly_court(heat_fig, layer='above')
         
-        make_player_hexbin(hex_fig,shots_df,name,season)
+        make_hexbin(hex_fig,shots_df,league_avg,'p',name,season)
         draw_plotly_court(hex_fig)
     else:
         draw_plotly_court(shot_fig)
@@ -436,6 +545,54 @@ def get_player_img(player):
         return url
     else:
         return 'https://stats.nba.com/media/img/league/nba-headshot-fallback.png'
+
+@app.callback(
+    [Output('player_name', 'children'),
+    Output('player_ht', 'children'),
+    Output('player_wt', 'children'),
+    Output('player_prior', 'children'),
+    Output('player_age', 'children'),
+    Output('player_born', 'children'),
+    Output('player_draft', 'children'),
+    Output('player_exp', 'children'),
+    Output('player_team_pos', 'children'),
+    Output('team_logo', 'src'),
+    Output('timeframe', 'children'),
+    Output('player_pts', 'children'),
+    Output('player_reb', 'children'),
+    Output('player_ast', 'children'),
+    Output('player_pie', 'children'),],
+    [Input('player', 'value')]
+)
+def get_player_common_info(player):
+    player_info = commonplayerinfo.CommonPlayerInfo(player).get_data_frames()[0]
+    name = player_info['DISPLAY_FIRST_LAST'][0]
+    prior = 'Prior: ' + player_info['LAST_AFFILIATION'][0]
+    height = 'Height: '+player_info['HEIGHT'][0]
+    weight = 'Weight: '+player_info['WEIGHT'][0] + 'lbs'
+    exp = 'Exp: ' + str(player_info['SEASON_EXP'][0])
+    jersey = '#' + player_info['JERSEY'][0] + ' '
+    team = ' | ' + player_info['TEAM_CITY'][0]+' '+player_info['TEAM_NAME'][0]
+    position = player_info['POSITION'][0]
+    abrv = player_info['TEAM_ABBREVIATION'][0]
+    team_logo= f'https://stats.nba.com/media/img/teams/logos/{abrv}_logo.svg'
+    birthDate=date(int(player_info['BIRTHDATE'][0][0:4]),int(player_info['BIRTHDATE'][0][5:7]),int(player_info['BIRTHDATE'][0][8:10]))
+    today=date.today()
+    age = 'Age: ' + str(today.year - birthDate.year - ((today.month, today.day) < (birthDate.month, birthDate.day)))
+    born = 'Born: ' + player_info['BIRTHDATE'][0][5:7]+'/'+player_info['BIRTHDATE'][0][8:10]+'/'+player_info['BIRTHDATE'][0][0:4]
+    draft ='Drafted: ' + player_info['DRAFT_YEAR'][0]+' Rnd '+player_info['DRAFT_ROUND'][0]+' Pick '+player_info['DRAFT_NUMBER'][0]
+
+    player_stats = commonplayerinfo.CommonPlayerInfo(player).get_data_frames()[1]
+    timeframe = player_stats['TimeFrame'][0].capitalize() + ' Stats'
+    if list(player_stats.columns)[-1] == 'PIE':
+        pie = 'PIE: ' + str(player_stats['PIE'][0])
+    else:
+        pie = 'All Star: ' + str(player_stats['ALL_STAR_APPEARANCES'][0])
+    pts = 'PTS: ' + str(player_stats['PTS'][0])
+    reb = 'REB: ' + str(player_stats['PTS'][0])
+    ast = 'AST: ' + str(player_stats['AST'][0])
+    
+    return [jersey,name], height, weight, prior, age, born, draft, exp, [position,team], team_logo, timeframe, pts, reb, ast, pie
 
 #### Run ###
 if __name__ == '__main__':
