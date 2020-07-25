7eb7496b195ac7ab75d789ad0e85045dbbf1b2cc
George <he.george10@gmail.com>
2020-07-24 22:09:40 -0400

    modified navbar

--- app/navbar.py
+++ app/navbar.py
@@ -1,4 +1,7 @@
 import dash_bootstrap_components as dbc
+import dash_html_components as html
+
+LOGO = "assets/SpotUp.svg"
 
 def header_colors():
     return {
@@ -7,27 +10,31 @@ def header_colors():
     }
 
 def Navbar():
-    navbar = dbc.NavbarSimple(
+    navbar = dbc.Navbar(
         children=[
-            dbc.NavItem(dbc.NavLink("Player Shot Chart ", href="/player-shot-chart")),
-            dbc.NavItem(dbc.NavLink("Team Shot Chart", href="/team-shot-chart")),
-            # dbc.DropdownMenu(
-            #     nav=True,
-            #     in_navbar=True,
-            #     label="Menu",
-            #     children=[
-            #         dbc.DropdownMenuItem("Entry 1"),
-            #         dbc.DropdownMenuItem("Entry 2"),
-            #         dbc.DropdownMenuItem(divider=True),
-            #         dbc.DropdownMenuItem("Entry 3"),
-            #             ],
-            #         ),
-                ],
-        brand="Home",
-        brand_href="/home",
+            html.A(
+                dbc.Row([
+                        dbc.Col(html.Img(src=LOGO, height="30px")),
+                        dbc.Col(html.Span("Spot-Up",style={'fontSize':'20px','display':'flex','flexWrap':'nowrap','width': 'max-content'}), className="ml-2"),
+                        ],
+                        align="center",
+                        no_gutters=True,
+                    ),
+                    href="/home",
+                ),
+            dbc.Col(html.Span(" ")),
+            html.Div( 
+                dbc.Row([
+                    dbc.NavLink("Player Shot Chart ", href="/player-shot-chart",style ={'color':'white'}),
+                    dbc.NavLink("Team Shot Chart", href="/team-shot-chart",style ={'color':'white'}),
+                ]))
+    
+        ],
+        
         sticky="top",
         color="black",
         dark=True,
+        style ={'height':'49px'}
     )
 
     return navbar
